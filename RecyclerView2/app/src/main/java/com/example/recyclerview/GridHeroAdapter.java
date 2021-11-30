5fe30dc7f8d7c9c6105624890b1d2f36ad77c05a
bimaafrizal <bimaafrizalmalna@student.uns.ac.id>
2021-11-30 13:37:56 +0700

    RecyclerView

---
+++ RecyclerView2/app/src/main/java/com/example/recyclerview/GridHeroAdapter.java
@@ -0,0 +1,65 @@
+package com.example.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class GridHeroAdapter extends RecyclerView.Adapter<GridHeroAdapter.GridViewHolder> {
+    private ArrayList<Hero> listHero;
+    private OnItemClickCallback onItemClickCallback;
+    public GridHeroAdapter(ArrayList<Hero> list) {
+        this.listHero = list;
+    }
+
+    void setOnItemClickCallback(OnItemClickCallback onItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    @NonNull
+    @Override
+    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_grid_hero,parent, false);
+        return new GridViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GridViewHolder holder, int position) {
+        Glide.with(holder.itemView.getContext())
+                .load(listHero.get(position).getPhoto())
+                .apply(new RequestOptions().override(350, 550))
+                .into(holder.imgPhoto);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onItemClickCallback.onItemClicked(listHero.get(holder.getAdapterPosition()));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listHero.size();
+    }
+
+    public class GridViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+
+        public GridViewHolder(View view) {
+            super(view);
+            imgPhoto = view.findViewById(R.id.img_item_photo);
+        }
+    }
+    public interface OnItemClickCallback {
+        void onItemClicked(Hero data);
+    }
+}
